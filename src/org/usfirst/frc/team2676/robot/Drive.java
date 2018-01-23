bfa7473b354fe238d5e42cdeee5ea13e13626efb
Mat Moradi <matmoradi17@gmail.com>
2018-01-23 13:08:45 -0800

    Added Basic SM Drive Configuration
    Will need to be updated to meet new CTRE standard libs, as these libraries are now depreciated.

--- src/org/usfirst/frc/team2676/robot/Drive.java
+++ src/org/usfirst/frc/team2676/robot/Drive.java
@@ -1,5 +1,23 @@
 package org.usfirst.frc.team2676.robot;
 
-public class Drive {
+import edu.wpi.first.wpilibj.CANTalon;
 
-}
+public class Drive {
+	public CANTalon LeftM, Left1, Left2, RightM, Right1, Right2;
+	
+	public Drive(){
+		LeftM = new CANTalon(RobotMap.DriveLeftM);
+		LeftS = new CANTalon(RobotMap.DriveLeftS);
+		LeftS.changeControlMode(CANTalon.TalonControlMode.Follower);
+		LeftS.set(RobotMap.DriveLeftM);
+		RightM = new CANTalon(RobotMap.DriveRightM);
+		RightS = new CANTalon(RobotMap.DriveRightS);
+		RightS.changeControlMode(CANTalon.TalonControlMode.Follower);
+		RightS.set(RobotMap.DriveRightM);
+	}
+	
+	public void drive(double left, double right){
+		LeftM.set(left);
+		RightM.set(-right);
+		}
+	}
