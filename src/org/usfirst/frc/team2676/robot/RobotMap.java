c6636b444c6763286c54f8b936f9c79afb403d58
Mat Moradi <matmoradi17@gmail.com>
2018-01-24 13:24:52 -0800

    Added RobotMap Class
    This should fixed the unrecognized references in drive and will need to be updated periodically and accoringly with additional subsystems. The values will definitely need to be changed though.

---
+++ src/org/usfirst/frc/team2676/robot/RobotMap.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2676.robot;
+
+public class RobotMap {
+	//This is the drive robot map.
+	public static final int DriveLeftM = 1;
+	public static final int DriveLeft1 = 2;
+	public static final int DriveRightM = 6;
+	public static final int DriveRight1 = 5;
+	
+	//Add more subsystems here.
+}
