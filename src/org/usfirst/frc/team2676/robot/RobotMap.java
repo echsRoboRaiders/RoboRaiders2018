505ae8f69de00b1c4d016d99e1bf78cc7e0fbd0c
Brett Hudson <Brett@brett-techrepair.com>
2018-02-03 15:21:17 -0500

    RoboMap Numbering Updated
    I have updated numbering given to me for the Robot Map

--- src/org/usfirst/frc/team2676/robot/RobotMap.java
+++ src/org/usfirst/frc/team2676/robot/RobotMap.java
@@ -2,9 +2,9 @@ package org.usfirst.frc.team2676.robot;
 
 public class RobotMap {
 	//This is the drive robot map.
-	public static final int DriveLeftM = 1;
-	public static final int DriveLeftS = 2;
-	public static final int DriveRightM = 6;
+	public static final int DriveLeftM = 6;
+	public static final int DriveLeftS = 7;
+	public static final int DriveRightM = 4;
 	public static final int DriveRightS = 5;
 	
 	//Add more subsystems here.
