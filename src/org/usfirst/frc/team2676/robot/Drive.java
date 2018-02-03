c6f18f1a5f9161a0e1bd16947776ce8d1bcb216f
Brett Hudson <Brett@brett-techrepair.com>
2018-02-02 19:41:58 -0500

    updated ctre updates on issue #1
    updated ctre updates on issue #1

--- src/org/usfirst/frc/team2676/robot/Drive.java
+++ src/org/usfirst/frc/team2676/robot/Drive.java
@@ -1,18 +1,18 @@
 package org.usfirst.frc.team2676.robot;
 
-import edu.wpi.first.wpilibj.CANTalon;
-
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class Drive {
-	public CANTalon LeftM, Left1, Left2, RightM, Right1, Right2;
+	public WPI_TalonSRX LeftM, Left1, Left2, RightM, Right1, Right2;
 	
 	public Drive(){
-		LeftM = new CANTalon(RobotMap.DriveLeftM);
-		LeftS = new CANTalon(RobotMap.DriveLeftS);
-		LeftS.changeControlMode(CANTalon.TalonControlMode.Follower);
+		LeftM = new WPI_TalonSRX(RobotMap.DriveLeftM);
+		LeftS = new WPI_TalonSRX(RobotMap.DriveLeftS);
+		LeftS.changeControlMode(WPI_TalonSRX.TalonControlMode.Follower);
 		LeftS.set(RobotMap.DriveLeftM);
-		RightM = new CANTalon(RobotMap.DriveRightM);
-		RightS = new CANTalon(RobotMap.DriveRightS);
-		RightS.changeControlMode(CANTalon.TalonControlMode.Follower);
+		RightM = new WPI_TalonSRX(RobotMap.DriveRightM);
+		RightS = new WPI_TalonSRX(RobotMap.DriveRightS);
+		RightS.changeControlMode(WPI_TalonSRX.TalonControlMode.Follower);
 		RightS.set(RobotMap.DriveRightM);
 	}
 	
