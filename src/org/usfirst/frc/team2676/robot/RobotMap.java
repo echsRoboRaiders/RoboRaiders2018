db3483797a079cc65e574140d9e8728428cd92d9
Mat Moradi <matmoradi17@gmail.com>
2018-02-03 23:14:43 -0800

    Added Solenoids To RobotMap
    Done in accordance to Week4 InfoUpdates as specified on the PCM.

--- src/org/usfirst/frc/team2676/robot/RobotMap.java
+++ src/org/usfirst/frc/team2676/robot/RobotMap.java
@@ -1,11 +1,15 @@
 package org.usfirst.frc.team2676.robot;
 
 public class RobotMap {
-	//This is the drive robot map.
+	//Drive Systems Map
 	public static final int DriveLeftM = 6;
 	public static final int DriveLeftS = 7;
 	public static final int DriveRightM = 4;
 	public static final int DriveRightS = 5;
 	
-	//Add more subsystems here.
+	//Solenoids Map
+	public static final int RightMotionSolenoid = 4;
+	public static final int LeftMotionSolenoid = 6;
+	public static final int RightFixedSolenoid = 5;
+	public static final int LeftFixedSolenoid = 7;
 }
